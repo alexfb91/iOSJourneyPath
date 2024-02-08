b6a71ace9064785d80c5ec80414bc822c83339de
alexfb91 <a_fajardito@hotmail.com>
2024-02-07 20:23:39 -0800

    Initial Commit Adding this is me

---
+++ iOSJourneyPath/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOSJourneyPath
+//
+//  Created by alejandro fajardo on 2024-02-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world This is me!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOSJourneyPath/iOSJourneyPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSJourneyPathApp.swift
+//  iOSJourneyPath
+//
+//  Created by alejandro fajardo on 2024-02-07.
+//
+
+import SwiftUI
+
+@main
+struct iOSJourneyPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
